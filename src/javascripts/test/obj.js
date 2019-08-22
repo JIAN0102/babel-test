43633fb91bcde0de1146c0e51befb8f7e45f8efe
JL Chen <azx011014@gmail.com>
2019-08-22 19:56:19 +0800

    export import

---
+++ src/javascripts/test/obj.js
@@ -0,0 +1,7 @@
+// module.exports = {
+//   name: 'jim'
+// }
+
+export default {
+  name: 'jimjim'
+}
--- src/javascripts/test/test.js
+++ src/javascripts/test/test.js
@@ -1,6 +1,12 @@
-const gulpbabeltest = 1;
-console.log(gulpbabeltest);
+// var obj = require('./obj.js');
+// console.log(obj);
 
-let newFn2 = () => {
-  console.log('a');
-}
+import obj from './obj.js';
+console.log(obj);
+
+// const gulpbabeltest = 1;
+// console.log(gulpbabeltest);
+
+// let newFn2 = () => {
+//   console.log('a');
+// }
